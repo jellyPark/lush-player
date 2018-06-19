5d40d5e3543063efe4b2746c98a2de64ccdd852a
jelly_kr <jelly@lush.co.kr>
2018-06-19 15:03:28 +0900

    test

--- src/main/java/com/lush/Application.java
+++ src/main/java/com/lush/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
 public class Application {
 
     @RequestMapping("/")
