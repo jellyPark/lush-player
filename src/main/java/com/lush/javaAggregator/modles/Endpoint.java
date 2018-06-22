34218ce0653210697da7eec343ad51ce97f52176
jelly_kr <jelly@lush.co.kr>
2018-06-22 09:29:05 +0900

    add endpoint

---
+++ src/main/java/com/lush/javaAggregator/modles/Endpoint.java
@@ -0,0 +1,47 @@
+package com.lush.javaAggregator.modles;
+
+import org.springframework.http.HttpMethod;
+
+/**
+ * Endpoint information.
+ */
+public class Endpoint {
+
+  /**
+   * Endpoint uri.
+   */
+  private String uri;
+
+  /**
+   * Http protocol method.
+   */
+  private HttpMethod method;
+
+  /**
+   *  Get Uri
+   */
+  public String getUri() {
+    return uri;
+  }
+
+  /**
+   *  Set Uri
+   */
+  public void setUri(String uri) {
+    this.uri = uri;
+  }
+
+  /**
+   *  Get Method
+   */
+  public HttpMethod getMethod() {
+    return method;
+  }
+
+  /**
+   *  Set Method
+   */
+  public void setMethod(HttpMethod method) {
+    this.method = method;
+  }
+}
