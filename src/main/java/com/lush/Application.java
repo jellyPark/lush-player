6acfa2d7ae68b03016258005e2d45b83cbadd84b
jelly_kr <jelly@lush.co.kr>
2018-06-25 19:50:51 +0900

    endpoint / and /healthz

--- src/main/java/com/lush/Application.java
+++ src/main/java/com/lush/Application.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Application {
-
-  @RequestMapping("/")
-  @ResponseBody
-  String home() {
-    return "Hello World!";
-  }
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
