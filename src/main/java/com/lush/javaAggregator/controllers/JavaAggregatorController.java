bfc3253de34ce160b549441c598589855ac27fa3
magic <magic@lush.co.kr>
2018-07-24 10:01:33 +0900

    add : getParams Util

--- src/main/java/com/lush/javaAggregator/controllers/JavaAggregatorController.java
+++ src/main/java/com/lush/javaAggregator/controllers/JavaAggregatorController.java
@@ -1,14 +1,9 @@
 package com.lush.javaAggregator.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lush.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,13 +12,20 @@ public class JavaAggregatorController {
   @Autowired
   private Util util;
 
+  @Autowired
+  private ObjectMapper mapper;
+
   @GetMapping("/findmethodtype")
   public String findMethodType() {
     return util.getMethodType();
   }
 
-  @GetMapping("/sampleGetUri")
-  public String sampleGetUri() {
-    return util.getUri();
-  }
+//  @PostMapping(value = "/testPodcasts")
+//  public String createPodcast(@RequestBody @Valid PodcastReq podcastReq) throws Exception {
+//
+//    HashMap reqMap = mapper.convertValue(podcastReq, HashMap.class);
+//    System.out.println("requestParams  :    " + reqMap.toString());
+//    return reqMap.toString();
+//  }
+
 }
--- src/main/java/com/lush/util/Util.java
+++ src/main/java/com/lush/util/Util.java
@@ -1,5 +1,9 @@
 package com.lush.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Enumeration;
+import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,5 +24,28 @@ public class Util {
     System.out.println("Request URL :: " + request.getRequestURL());
     System.out.println("Request URI :: " + request.getRequestURI());
     return request.getRequestURI();
+  }
+
+  public HashMap getParams(String params) throws Exception{
+    HashMap reqMap = new HashMap();
+    if(params.equals("")){
+      System.out.println("requestParams is null");
+    }else{
+      String methodType = request.getMethod();
+      if(methodType.equals("POST") || methodType.equals("PUT")){
+        ObjectMapper mapper = new ObjectMapper();
+        reqMap = mapper.readValue(params, new TypeReference<HashMap>(){});
+      }else{
+        Enumeration e= request.getParameterNames();
+        while (e.hasMoreElements()) {
+          String strKey = (String)e.nextElement();
+          Object strVal[] = request.getParameterValues(strKey);
+          if( !reqMap.containsKey( strKey ) ){
+            reqMap.put(strKey,strVal[0]);
+          }
+        }
+      }
+    }
+    return reqMap;
   }
 }
