bd6ca3cb5f0fff39cb4a6f3a159e3b69127e6c1c
jelly <jelly@lush.co.kr>
2018-10-11 15:53:39 +0900

    modify call service

--- src/main/java/com/lush/javaAggregator/controllers/JavaAggregatorController.java
+++ src/main/java/com/lush/javaAggregator/controllers/JavaAggregatorController.java
@@ -5,7 +5,12 @@ import com.lush.javaAggregator.exceptions.BaseException;
 import com.lush.javaAggregator.modles.Response;
 import com.lush.javaAggregator.utils.HttpUtil;
 import com.lush.util.Util;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.Scanner;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,13 +57,14 @@ public class JavaAggregatorController {
     Response response = new Response();
 
     //get uri
-    String url = util.getUri();
-    System.out.println("####  url :   " + url);
-    System.out.println("####  request : " + request.getMethod());
+    logger.info("####  request : " + request.getMethod());
     System.out.println("#### TEST");
 
     //setRequest(토큰추출 및 리퀘스트가공)
     //Map<String, Object> setRequest = util.setRequest(request);
+//    String header = request.getHeader("Authorization"); // 토큰 추출
+    // Request Parameter 의 내용은 각 서비스에서 그에 맞게 가공 필요
+
 
     //setEndpoint(엔드포인트 추출)
     String endpoint = "";
@@ -87,21 +93,23 @@ public class JavaAggregatorController {
    * @return ResponseEntity
    */
   @PostMapping
-  public ResponseEntity<Object> post(@RequestBody Map<String, Object> params) {
+  public ResponseEntity<Object> post(@RequestBody Map<String, Object> params) throws Exception{
 
-    System.out.println("#### request :  " + request.getMethod());
-    System.out.println("#### params :  " + params.toString());
 
     Response response = new Response();
 
     //get uri
-    String url = util.getUri();
-    System.out.println("####  url :   " + url);
-    System.out.println("####  request : " + request.getMethod());
+    System.out.println("#### request :  " + request.getMethod());
+    System.out.println("#### params :  " + params.toString());
     System.out.println("#### TEST");
 
     //setRequest(토큰추출 및 리퀘스트가공)
     //Map<String, Object> setRequest = util.setRequest(request);
+    Enumeration bodtyParams = request.getParameterNames();
+    while(bodtyParams.hasMoreElements()){
+      String paramName = (String)bodtyParams.nextElement();
+      System.out.println("##############" + paramName + " = " + request.getParameter(paramName));
+    }
 
     //setEndpoint(엔드포인트 추출)
     String endpoint = "";
@@ -129,8 +137,6 @@ public class JavaAggregatorController {
     Response response = new Response();
 
     //get uri
-    String url = util.getUri();
-    System.out.println("####  url :   " + url);
     System.out.println("####  request : " + request.getMethod());
     System.out.println("#### TEST");
 
@@ -162,8 +168,6 @@ public class JavaAggregatorController {
     Response response = new Response();
 
     //get uri
-    String url = util.getUri();
-    System.out.println("####  url :   " + url);
     System.out.println("####  request : " + request.getMethod());
     System.out.println("#### TEST");
 
--- src/main/java/com/lush/util/Util.java
+++ src/main/java/com/lush/util/Util.java
@@ -6,13 +6,10 @@ import com.lush.javaAggregator.enums.ExceptionType;
 import com.lush.javaAggregator.enums.ResponseStatusType;
 import com.lush.javaAggregator.exceptions.BaseException;
 import com.lush.javaAggregator.modles.Audio;
-import com.lush.javaAggregator.modles.Podcast;
 import com.lush.javaAggregator.modles.Response;
-import com.lush.javaAggregator.modles.Token;
 import java.net.HttpURLConnection;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.modelmapper.ModelMapper;
@@ -31,9 +28,6 @@ import org.springframework.web.client.RestTemplate;
 public class Util {
 
   static final Logger logger = LoggerFactory.getLogger(Util.class);
-
-  @Autowired
-  private HttpServletRequest request;
 
   @Value("${services.domain}")
   private String domain;
@@ -57,68 +51,49 @@ public class Util {
   private ModelMapper modelMapper;
 
   /**
-   * Get a login token.
-   *
-   * @return token value.
-   */
-  // 추후 해당 내용 삭제 -> Header에서 넘어오는 tokenKey를 추출하여 사용하는 방식으로 변경 진행 해야함.
-  public String login() {
-
-    RestTemplate restTemplate = new RestTemplate();
-    Map<String, Object> loginInfo = new HashMap<>();
-    loginInfo.put("email", "webdev@lush.co.uk");
-    loginInfo.put("password", "13 young START quarter 75");
-
-    String targetUri = "https://" + gateway_uri + "-" + environment + "." + domain + "/login";
-    Map<String, Object> objectMap = restTemplate.postForObject(targetUri, loginInfo, Map.class);
-
-    Token key = getTokenKey(objectMap);
-
-    return key.getValue();
-  }
-
-
-  /**
    * Method name : callService.
    * Description : Call the service.
    *
-   * @param endpoint
-   * @param tokenKey
-   * @param parameter
+   * @param url
+   * @param request
+   * @param methodType
    * @return
    */
-  // parameter로 endpoint를 받아야할지, 아니면 getUri()를 사용할 것인지 논의 필요.
-  // 현재 endpoint를 받아오는 이유는 두개의 endpoint를 호출해야 하는 경우가 발생하기 때문.
-  public Map<String, Object> callService(String endpoint, String tokenKey,
-      Map<String, Object> parameter) {
+  public Map<String, Object> callService(String url, HttpServletRequest request, String methodType) {
 
     RestTemplate restTemplate = new RestTemplate();
     Integer responseCode = 200;
     String errorMessage = "";
-    String targetURL = setServiceURL(endpoint);
     Map<String, Object> objectMap = null;
+
     // Add request header.
-    String methodType = getMethodType();
     HttpHeaders headers = new HttpHeaders();
-    headers.set("Authorization", "Bearer " + tokenKey);
 
     try {
       if ("POST".equals(methodType) || "PUT".equals(methodType)) {
-        HttpEntity<Object> httpEntity = new HttpEntity<Object>(parameter, headers);
+
+        HttpEntity<Object> httpEntity = new HttpEntity<Object>(request);
+
         ResponseEntity<Object> response = restTemplate
-            .exchange(targetURL, HttpMethod.resolve(methodType), httpEntity, Object.class);
+            .exchange(url, HttpMethod.resolve(methodType), httpEntity, Object.class);
+
         responseCode = response.getStatusCodeValue();
         ObjectMapper mapper = new ObjectMapper();
         objectMap = mapper.convertValue(response.getBody(), Map.class);
+
       } else {
+
         HttpEntity<String> httpEntity = new HttpEntity<String>(headers);
+
         ResponseEntity<Object> response = restTemplate
-            .exchange(targetURL, HttpMethod.resolve(methodType), httpEntity, Object.class);
+            .exchange(url, HttpMethod.resolve(methodType), httpEntity, Object.class);
+
         responseCode = response.getStatusCodeValue();
         ObjectMapper mapper = new ObjectMapper();
         objectMap = mapper.convertValue(response.getBody(), Map.class);
       }
-      logger.info("Sending '" + methodType + "' request to URL : " + targetURL);
+
+      logger.info("Sending '" + methodType + "' request to URL : " + url);
 
       if (responseCode == HttpURLConnection.HTTP_OK) {
         return objectMap;
@@ -198,61 +173,6 @@ public class Util {
     return audios;
   }
 
-  /**
-   * Formatting Map object to Podcast object.
-   *
-   * @param response
-   * @param audios
-   * @return Podcast
-   */
-  public Podcast bindingPodcasts(Map<String, Object> response, List<Audio> audios) {
-
-    Podcast podcast = new Podcast();
-
-    if (response.containsKey("data")) {
-      Map<String, Object> data = (Map<String, Object>) response.get("data"); // data만 꺼내옴
-
-      if (data.containsKey("podcasts")) {
-        Map<String, Object> podcasts = (Map<String, Object>) data.get("podcasts"); // podcasts 꺼내옴
-
-        podcast = modelMapper.map(podcasts, Podcast.class);
-        podcast.setAudio_files(audios);
-
-      }
-    }
-
-    return podcast;
-
-  }
-
-  /**
-   * Formatting Map object to Token objcet.
-   *
-   * @param parameter
-   * @return Token
-   */
-  public Token getTokenKey(Map<String, Object> parameter) {
-
-    Token token = new Token();
-
-    if (parameter.containsKey("data")) {
-      Map<String, Object> data = (Map<String, Object>) parameter.get("data"); // data만 꺼내옴
-
-      if (data.containsKey("consumer")) {
-        Map<String, Object> consumer = (Map<String, Object>) data.get("consumer"); // consumer만 꺼내옴
-
-        if (consumer.containsKey("tokens")) {
-
-          List<Map<String, Object>> tokens = (List<Map<String, Object>>) consumer.get("tokens");
-          ObjectMapper m = new ObjectMapper();
-          token = m.convertValue(tokens.get(0), Token.class);
-
-        }
-      }
-    }
-
-    return token;
-  }
 
   /**
    * Method name : bindingJson.
@@ -280,15 +200,15 @@ public class Util {
    * Utils
    *******************************************************/
 
-  /**
-   * Method name : getUri.
-   * Description : Get the endpoint.
-   *
-   * @return String
-   */
-  public String getUri() {
-    return request.getRequestURI();
-  }
+//  /**
+//   * Method name : getUri.
+//   * Description : Get the endpoint.
+//   *
+//   * @return String
+//   */
+//  public String getUri() {
+//    return request.getRequestURI();
+//  }
 
   /**
    * Method name : setServiceURL.
@@ -305,58 +225,58 @@ public class Util {
     }
   }
 
-  /**
-   * Method name : getMethodType.
-   * Description : Get the method type of RequestMapping.
-   *
-   * @return String
-   */
-  public String getMethodType() {
-    return request.getMethod();
-  }
+//  /**
+//   * Method name : getMethodType.
+//   * Description : Get the method type of RequestMapping.
+//   *
+//   * @return String
+//   */
+//  public String getMethodType() {
+//    return request.getMethod();
+//  }
 
 
-  /**
-   * Method name : getParams.
-   * Description :
-   *
-   * @return Map
-   */
-  public Map<String, Object> getParams(Map<String, Object> params) throws Exception {
-    Map<String, Object> reqMap = new HashMap<String, Object>();
-
-    if (params.equals("")) {
-      logger.info("requestParams is null");
-    } else {
-      String methodType = request.getMethod();
-      if (methodType.equals("POST") || methodType.equals("PUT")) {
-        reqMap.putAll(params);
-      } else {
-        Enumeration e = request.getParameterNames();
-        while (e.hasMoreElements()) {
-          String strKey = (String) e.nextElement();
-          Object strVal[] = request.getParameterValues(strKey);
-          if (!reqMap.containsKey(strKey)) {
-            reqMap.put(strKey, strVal[0]);
-          }
-        }
-      }
-    }
-    return reqMap;
-  }
-
-  public boolean checkPageNum() throws Exception {
-    boolean check = true;
-    String methodType = request.getMethod();
-    System.out.println("  request.getParameter : " + request.getParameter("page"));
-    if (methodType.equals("GET")) {
-      String pageNum = request.getParameter("page");
-      if (pageNum != null) {
-        if (Integer.parseInt(pageNum) < 0) {
-          check = false;
-        }
-      }
-    }
-    return check;
-  }
+//  /**
+//   * Method name : getParams.
+//   * Description :
+//   *
+//   * @return Map
+//   */
+//  public Map<String, Object> getParams(Map<String, Object> params) throws Exception {
+//    Map<String, Object> reqMap = new HashMap<String, Object>();
+//
+//    if (params.equals("")) {
+//      logger.info("requestParams is null");
+//    } else {
+//      String methodType = request.getMethod();
+//      if (methodType.equals("POST") || methodType.equals("PUT")) {
+//        reqMap.putAll(params);
+//      } else {
+//        Enumeration e = request.getParameterNames();
+//        while (e.hasMoreElements()) {
+//          String strKey = (String) e.nextElement();
+//          Object strVal[] = request.getParameterValues(strKey);
+//          if (!reqMap.containsKey(strKey)) {
+//            reqMap.put(strKey, strVal[0]);
+//          }
+//        }
+//      }
+//    }
+//    return reqMap;
+//  }
+//
+//  public boolean checkPageNum() throws Exception {
+//    boolean check = true;
+//    String methodType = request.getMethod();
+//    System.out.println("  request.getParameter : " + request.getParameter("page"));
+//    if (methodType.equals("GET")) {
+//      String pageNum = request.getParameter("page");
+//      if (pageNum != null) {
+//        if (Integer.parseInt(pageNum) < 0) {
+//          check = false;
+//        }
+//      }
+//    }
+//    return check;
+//  }
 }
